1b39a54c885e90844f592961c97a8c4c6c54b473
Rob Harper <robert.harper@gmail.com>
2013-01-14 22:06:32 -0500

    Easier handling of js assets - another static route

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var express = require('express')
   , path = require('path');
 
 require('coffee-script');
-require('./lib/noop-compiler');
 var routes = require('./routes')
 
 var app = express();
@@ -18,26 +17,36 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  
   app.use(compiler({
-    enabled : [ 'coffee', 'stylus', 'js-noop' ],
+    enabled : [ 'coffee', 'stylus' ],
     src     : 'assets',
     dest    : 'var'
   }));
+  
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'var')));
+
+  // Three places to find static files :(
+  // Public
   app.use(express.static(path.join(__dirname, 'public')));
+  // Compiled styl / coffee
+  app.use(express.static(path.join(__dirname, 'var')));
+  // Straight-up no-compilation necessary assets like js libs
+  app.use(express.static(path.join(__dirname, 'assets')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-// app.get('/', routes.index);
+// Routes
 app.get('/weather', routes.weather);
 
+// Start
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
